4d2032587e836fc5e10a9a15ea35825b7c3441e5
Petru <petru@Petrus-MacBook-Pro.local>
2023-01-09 14:13:27 +0200

    Remove blog pages and add JSON pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -60,37 +60,37 @@ exports.createPages = async ({ graphql, actions }) => {
     const blogAllPostsTemplate = path.resolve(`src/template/blogAllPostsTemplate.js`);
 
     // JSON Pages
-    // await graphql(`
-    //     query MyQuery {
-    //         allFile(filter: { extension: { eq: "json" } }) {
-    //             edges {
-    //                 node {
-    //                     childrenJson {
-    //                         sections
-    //                         seoTitle
-    //                         title
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // `).then((result) => {
-    //     if (result.errors) throw result.errors;
-    //
-    //     const posts = result.data.allFile.edges;
-    //
-    //     posts.forEach((edge) => {
-    //         const node = edge.node;
-    //         createPage({
-    //             // Path for this page — required
-    //             path: '/pagesjson/' + node.childrenJson[0].seoTitle + '/',
-    //             component: jsonTemplate,
-    //             context: {
-    //                 alldata: node.childrenJson[0]
-    //             }
-    //         });
-    //     });
-    // });
+    await graphql(`
+        query MyQuery {
+            allFile(filter: { extension: { eq: "json" } }) {
+                edges {
+                    node {
+                        childrenJson {
+                            sections
+                            seoTitle
+                            title
+                        }
+                    }
+                }
+            }
+        }
+    `).then((result) => {
+        if (result.errors) throw result.errors;
+
+        const posts = result.data.allFile.edges;
+
+        posts.forEach((edge) => {
+            const node = edge.node;
+            createPage({
+                // Path for this page — required
+                path: '/pagesjson/' + node.childrenJson[0].seoTitle + '/',
+                component: jsonTemplate,
+                context: {
+                    alldata: node.childrenJson[0]
+                }
+            });
+        });
+    });
 
     // Markdown Pages
     await graphql(`
@@ -125,150 +125,6 @@ exports.createPages = async ({ graphql, actions }) => {
                     alldata: node
                 }
             });
-        });
-    });
-
-    const whitepaperCover = await graphql(`
-        query WhitepaperCover {
-            whitepaperCover: file(relativePath: { eq: "whitepaper-cover-blog.png" }) {
-                childImageSharp {
-                    gatsbyImageData(width: 111, height: 162, layout: FIXED)
-                }
-            }
-        }
-    `);
-
-    await graphql(
-        `
-            query loadPostsQuery {
-                allMarkdownRemark(
-                    sort: { order: DESC, fields: frontmatter___date }
-                    filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-                ) {
-                    edges {
-                        node {
-                            frontmatter {
-                                authors
-                                categories
-                                title
-                                seoTitle
-                                description
-                                seoDescription
-                                date
-                                featuredpost
-                                permalink
-                                featuredimage {
-                                    publicURL
-                                    childImageSharp {
-                                        gatsbyImageData(width: 820, layout: CONSTRAINED)
-                                    }
-                                }
-                            }
-                            rawMarkdownBody
-                        }
-                    }
-                }
-            }
-        `
-    ).then(async (result) => {
-        if (result.errors) throw result.errors;
-
-        const allPosts = [];
-        const posts = result.data.allMarkdownRemark.edges;
-
-        posts.forEach((edge) => {
-            const node = edge.node;
-            allPosts.push(edge.node);
-            // Get all distinct categories
-            node.frontmatter.categories.forEach((category) => {
-                if (!postsByCategory[category]) {
-                    postsByCategory[category] = [node];
-                } else {
-                    postsByCategory[category].push(node);
-                }
-            });
-
-            createPage({
-                // Path for this page — required
-                path: '/blog/' + node.frontmatter.permalink + '/',
-                component: blogTemplate,
-                context: {
-                    alldata: node,
-                    suggestions: [posts[0], posts[1], posts[2], posts[3]],
-                    whitepaperCover: whitepaperCover
-                }
-            });
-        });
-
-        const categoriesWithPosts = Object.keys(postsByCategory);
-        const numPages = Math.ceil(allPosts.length / postsPerPage);
-        Array.from({ length: numPages }).forEach((_, i) => {
-            createPage({
-                path: i === 0 ? `/blog` : `/blog/${i + 1}`,
-                component: blogAllPostsTemplate,
-                context: {
-                    limit: postsPerPage,
-                    skip: i * postsPerPage,
-                    numPages,
-                    currentPage: i + 1,
-                    category: 'All',
-                    seoTitle: i === 0 ? 'Blog' : `Blog Page ${i + 1}`,
-                    seoDescription: 'Cloud and Data Security Blog',
-                    categoriesList: categoriesWithPosts
-                }
-            });
-        });
-    });
-
-    await graphql(
-        `
-            query loadCategoriesQuery {
-                allMarkdownRemark(filter: { frontmatter: { templateKey: { eq: "category" } } }) {
-                    edges {
-                        node {
-                            frontmatter {
-                                name
-                                slug
-                                seoTitle
-                                seoDescription
-                                disabled
-                            }
-                        }
-                    }
-                }
-            }
-        `
-    ).then((result) => {
-        if (result.errors) throw result.errors;
-
-        const categories = result.data.allMarkdownRemark.edges;
-        const categoriesWithPosts = Object.keys(postsByCategory);
-        categories.forEach((edge) => {
-            const node = edge.node;
-
-            if (!node.frontmatter.disabled) {
-                // Create category pages
-                const numPages = Math.ceil(postsByCategory[node.frontmatter.name]?.length / postsPerPage) || 0;
-                Array.from({ length: numPages }).forEach((_, i) => {
-                    createPage({
-                        path:
-                            i === 0
-                                ? `/blog/${node.frontmatter.name.toLowerCase()}`
-                                : `/blog/${node.frontmatter.name.toLowerCase()}/${i + 1}`,
-                        component: path.resolve(`src/template/blogCategoriesTemplate.js`),
-                        context: {
-                            limit: postsPerPage,
-                            skip: i * postsPerPage,
-                            numPages,
-                            currentPage: i + 1,
-                            category: node.frontmatter.name,
-                            seoTitle: node.frontmatter.seoTitle,
-                            seoDescription: node.frontmatter.seoDescription,
-                            categoriesList: categoriesWithPosts
-                        }
-                    });
-                });
-            }
         });
     });
 };
